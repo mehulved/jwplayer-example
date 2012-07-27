2e426db0c42111d5d85f203383357813dd935925
Mehul Ved <mehul.n.ved@gmail.com>
2012-07-28 00:32:35 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<script type='text/javascript' src='jwplayer/jwplayer.js'></script>
+</head>
+
+<body>
+<div id='container'>Loading the player...</div>
+<script type='text/javascript'>
+jwplayer('container').setup({
+  flashplayer: 'jwplayer/player.swf',
+  file:'http://www.youtube.com/watch?v=bBEUOZ1-_gE',
+  height: 400,
+  width: 600,
+  'playlist.position': 'right',
+  'playlist.size': 80
+  })
+
+</script>
+</body>
+</html>
